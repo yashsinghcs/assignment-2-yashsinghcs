63549be9a3c115e83df675f848b30379bcebfe49
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-03-28 14:07:33 +0530

    add method to insert in treequeue

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,29 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
+import java.util.Queue;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    static Queue<TreeNode> q;
+
+    public TreeNode insert(TreeNode root, int x) {
+
+        if (root == null)
+
+            return new TreeNode(x);
+
+        else if (x > root.getData()) // x is greater. Should be inserted to right
+
+            root.setRight(insert(root.getRight(), x));
+
+        else // x is smaller should be inserted to left
+
+            root.setLeft(insert(root.getLeft(), x));
+
+        return root;
+    }
 }
