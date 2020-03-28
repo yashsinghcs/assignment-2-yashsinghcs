33878bf5b7b981b9691d71edfbfc517d79263b30
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-03-28 19:52:03 +0530

    CREATE Node class

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,32 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+
+import problem3.Student;
+
 // to define node properties
 public class Node {
+    private Student data;
+    private Node next;
+
+    public Node(Student data) {
+        this.data = data;
+        next = null;
+    }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
