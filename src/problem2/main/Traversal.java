a997c8e1b27e1625a0b393a83bccacd809edd763
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-03-28 15:49:28 +0530

    complete Traversal class

--- src/problem2/main/Traversal.java
+++ src/problem2/main/Traversal.java
@@ -1,4 +1,48 @@
 package problem2.main;
 
+import problem1.node.TreeNode;
+
 public class Traversal {
+    void printPostorder(TreeNode node) {
+
+        if (node == null)
+
+            return;
+
+        printPostorder(node.getLeft());
+
+        printPostorder(node.getRight());
+
+        System.out.print(node.getData() + " ");
+
+    }
+
+    void printInorder(TreeNode node) {
+
+        if (node == null)
+
+            return;
+
+        printInorder(node.getLeft());
+
+        System.out.print(node.getData() + " ");
+
+        printInorder(node.getRight());
+
+    }
+
+    void printPreorder(TreeNode node) {
+
+        if (node == null)
+
+            return;
+
+        System.out.print(node.getData() + " ");
+
+        printPreorder(node.getLeft());
+
+        printPreorder(node.getRight());
+
+    }
+
 }
