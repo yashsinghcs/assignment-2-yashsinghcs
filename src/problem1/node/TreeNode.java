d1aa53080ec541b25773aeccf1dc456084f60d93
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-03-28 14:00:55 +0530

    add getter setter TreeNode

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -17,4 +17,27 @@ public class TreeNode {
         right = null;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
