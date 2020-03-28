66ea5bedca2eafb0a999fd276bdbecd65af483b2
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-03-28 14:34:48 +0530

    complete MyMain for problem 1

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,40 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyBinarySearchTree mst = new MyBinarySearchTree();
+
+        TreeNode root;
+
+        root = new TreeNode(10);
+
+        mst.insert(root, 5);
+
+        mst.insert(root, 1);
+
+        mst.insert(root, 15);
+
+        mst.insert(root, 9);
+
+        mst.insert(root, 7);
+
+        mst.insert(root, 12);
+
+        mst.insert(root, 30);
+
+        mst.insert(root, 25);
+
+        mst.insert(root, 40);
+
+        mst.insert(root, 45);
+
+        mst.insert(root, 6);
+        mst.leftView(root);
+
+    }
 }
