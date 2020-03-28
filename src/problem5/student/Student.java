d71429d0a421f2e3e3536a8592794672638c67d6
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-03-28 19:46:43 +0530

    CREATE Student class

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,27 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private int backlogCount;
+    private String studentName;
+
+    public Student(String studentName, int back) {
+        this.studentName = studentName;
+        backlogCount = back;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getBacklogCount() {
+        return backlogCount;
+    }
+
+    public void setBacklogCount(int backlogCount) {
+        this.backlogCount = backlogCount;
+    }
 }
