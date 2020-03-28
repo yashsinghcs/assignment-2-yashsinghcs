e8ef4b4ddd66afaf65d04ef649ee55d7ef78695c
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-03-28 13:59:33 +0530

    add constructor TreeNode

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,11 @@ public class TreeNode {
     private int data;
     private TreeNode left;
     private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
 }
